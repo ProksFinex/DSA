52f51e57f7a2eeae39b7a9fce9f235b4322a246f
ProksFinex <prabhatjnu2@gmail.com>
2025-07-07 17:36:59 +0530

    checkPalindrome

---
+++ Math/palindrome.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool CheckPalindrome(int n){
+
+    if(n<0){
+        return false;
+    }
+    
+    int rn=0;
+    int dup=n;
+    
+    while(n>0){
+        int ld=n%10;
+
+        if((rn>INT_MAX/10)||(rn<INT_MIN/10)){
+            return 0;
+        }
+
+        rn=(rn*10)+ld;
+        n/=10;
+    }
+    // if(dup==rn) cout<<"True";
+    // else cout<<"False";
+    return dup==rn;
+}
+
+int main(){
+    int n=0;
+    cin>>n;
+    CheckPalindrome(n);
+
+
+    return 0;
+}
