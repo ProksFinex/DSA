3dfda351dee2f423ad07cd168973694d3353b35d
ProksFinex <prabhatjnu2@gmail.com>
2025-07-10 01:46:21 +0530

    armstrong number

---
+++ Math/armstrongNumber.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool armstorngNum(int n)
+{
+    int duplicate = n;
+    int sum = 0;
+    while (n > 0)
+    {
+        int ld = n % 10;
+        sum = sum + (ld * ld * ld);
+        n /= 10;
+    }
+    return duplicate == sum;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout<<boolalpha<<armstorngNum(n);
+    return 0;
+
+}
